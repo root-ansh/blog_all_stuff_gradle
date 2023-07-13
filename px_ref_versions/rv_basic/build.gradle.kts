26b511f946bcfd5dee66e7b952292261da45460e
asachde1 <ansh1.sachdeva@tataaig.com>
2023-07-13 17:21:08 +0530

    update agp to 8.0.2

--- px_ref_versions/rv_basic/build.gradle.kts
+++ px_ref_versions/rv_basic/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") apply(false)
-    id("com.android.library") apply(false)
+    id("com.android.application") apply (false) version "8.0.2"
+    id("com.android.library") apply (false) version "8.0.2"
     id("org.jetbrains.kotlin.android") apply(false)
 
 
